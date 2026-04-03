ff06e587eccc8d3163e08e42aa336321efcba011
CulturalProfessor <svinayak994@gmail.com>
2026-04-04 02:31:12 +0530

    fix : sdl opengl setup

--- main.cpp
+++ main.cpp
@@ -2,53 +2,64 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int gScreenWidth = 640;
+int gScreenHeight = 480;
+SDL_Window *gGraphicsApplicationWindow = nullptr;
+SDL_GLContext gOpenGLContext = nullptr;
+bool gQuit = false;
+
+void initializeProgram() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    std::cout << "SDL Error: " << SDL_GetError() << std::endl;
-    return 1;
+    std::cout << "SDL2 not initialized" << std::endl;
+    exit(1);
   }
 
-  // Tell SDL we want OpenGL
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-  SDL_Window *window =
-      SDL_CreateWindow("OpenGL Window", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 1000, 600, SDL_WINDOW_OPENGL);
-
-  if (!window) {
-    std::cout << SDL_GetError() << std::endl;
-    return 1;
+  gGraphicsApplicationWindow = SDL_CreateWindow(
+      "OPENGL WINDOW", 0, 0, gScreenWidth, gScreenHeight, SDL_WINDOW_OPENGL);
+  if (gGraphicsApplicationWindow == nullptr) {
+    std::cout << "SDL_WINDOW_OPENGL not initialized" << std::endl;
+    exit(1);
   }
 
-  SDL_GLContext glContext = SDL_GL_CreateContext(window);
-
-  if (!glContext) {
-    std::cout << "OpenGL Context Error: " << SDL_GetError() << std::endl;
-    return 1;
+  gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow);
+  if (gOpenGLContext == nullptr) {
+    std::cout << "OpenGL Context not initialized" << std::endl;
+    exit(1);
   }
+}
 
-  bool running = true;
-  SDL_Event event;
-
-  while (running) {
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        running = false;
-      }
+void input() {
+  SDL_Event e;
+  while (SDL_PollEvent(&e) != 0) {
+    if (e.type == SDL_QUIT) {
+      std::cout << "Exiting" << std::endl;
+      gQuit = true;
     }
-
-    // OpenGL rendering
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Swap buffers
-    SDL_GL_SwapWindow(window);
   }
+}
+void preDraw() {}
+void draw() {}
 
-  SDL_GL_DeleteContext(glContext);
-  SDL_DestroyWindow(window);
+void mainLoop() {
+  while (!gQuit) {
+    input();
+    SDL_GL_SwapWindow(gGraphicsApplicationWindow);
+  }
+}
+void cleanUp() {
+  SDL_DestroyWindow(gGraphicsApplicationWindow);
   SDL_Quit();
+}
+
+int main() {
+  initializeProgram();
+  mainLoop();
+  cleanUp();
   return 0;
 }
